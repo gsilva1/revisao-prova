e1621161d25dc460307cdd4853e1afe3b1030c9b
Guilherme O. Silva <osilva.guilherme@gmail.com>
2019-10-15 13:40:13 -0300

    pom atualizado2

---
+++ revisaoapp/src/main/java/br/edu/ifrs/canoas/lpoo2/revisaoapp/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.canoas.lpoo2.revisaoapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
